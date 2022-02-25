5129bc8699e15bf55206ac7884e19677f6396ffb
tomatome <362521329@qq.com>
2022-02-25 18:13:58 +0800

    keyboard

--- example/ui.go
+++ example/ui.go
@@ -54,20 +54,12 @@ func appMain(driver gxui.Driver) {
 	layoutImg.OnMouseScroll(func(e gxui.MouseEvent) {
 		//gc.MouseWheel(e.ScrollY, e.Point.X, e.Point.Y)
 	})
-	layoutImg.OnKeyDown(func(e gxui.KeyboardEvent) {
-		fmt.Println("layoutImg OnKeyDown:", int(e.Key))
-		key := int(e.Key)
-		if key == 52 {
-			key = 65293
-		}
+	window.OnKeyDown(func(e gxui.KeyboardEvent) {
+		key := transKey(e.Key)
 		gc.KeyDown(key, "")
 	})
-	layoutImg.OnKeyUp(func(e gxui.KeyboardEvent) {
-		fmt.Println("layoutImg OnKeyUp:", int(e.Key))
-		key := int(e.Key)
-		if key == 52 {
-			key = 65293
-		}
+	window.OnKeyUp(func(e gxui.KeyboardEvent) {
+		key := transKey(e.Key)
 		gc.KeyUp(key, "")
 	})
 
@@ -85,10 +77,10 @@ func appMain(driver gxui.Driver) {
 	user.SetDesiredWidth(width / 4)
 	passwd.SetDesiredWidth(width / 4)
 	//ip.SetText("192.168.18.100:5902")
-	//ip.SetText("192.168.2.207:3389")
+	ip.SetText("192.168.18.207:3389")
 	//user.SetText("administrator")
-	//user.SetText("test")
-	//passwd.SetText("test")
+	user.SetText("wren")
+	passwd.SetText("wren")
 
 	bok := theme.CreateButton()
 	bok.SetText("OK")
@@ -282,224 +274,117 @@ type Control interface {
 	Close()
 }
 
-/*func translateKeyboardKey(in gxui.KeyboardKey) int {
-	switch in {
-	case gxui.KeySpace:
-		return glfw.KeySpace
-	case gxui.KeyApostrophe:
-		return glfw.KeyApostrophe
-	case gxui.KeyComma:
-		return glfw.KeyComma
-	case gxui.KeyMinus:
-		return glfw.KeyMinus
-	case gxui.KeyPeriod:
-		return glfw.KeyPeriod
-	case gxui.KeySlash:
-		return glfw.KeySlash
-	case gxui.Key0:
-		//return glfw.Key0
-		return glfw.Key(11)
-	case gxui.Key1:
-		return 0x2
-	case gxui.Key2:
-		return glfw.Key2
-	case gxui.Key3:
-		return glfw.Key3
-	case gxui.Key4:
-		return glfw.Key4
-	case gxui.Key5:
-		return glfw.Key5
-	case gxui.Key6:
-		return glfw.Key6
-	case gxui.Key7:
-		return glfw.Key7
-	case gxui.Key8:
-		return glfw.Key8
-	case gxui.Key9:
-		return glfw.Key9
-	case gxui.KeySemicolon:
-		return glfw.KeySemicolon
-	case gxui.KeyEqual:
-		return glfw.KeyEqual
-	case gxui.KeyA:
-		return glfw.KeyA
-	case gxui.KeyB:
-		return glfw.KeyB
-	case gxui.KeyC:
-		return glfw.KeyC
-	case gxui.KeyD:
-		return glfw.KeyD
-	case gxui.KeyE:
-		return glfw.KeyE
-	case gxui.KeyF:
-		return glfw.KeyF
-	case gxui.KeyG:
-		return glfw.KeyG
-	case gxui.KeyH:
-		return glfw.KeyH
-	case gxui.KeyI:
-		return glfw.KeyI
-	case gxui.KeyJ:
-		return glfw.KeyJ
-	case gxui.KeyK:
-		return glfw.KeyK
-	case gxui.KeyL:
-		return glfw.KeyL
-	case gxui.KeyM:
-		return glfw.KeyM
-	case gxui.KeyN:
-		return glfw.KeyN
-	case gxui.KeyO:
-		return glfw.KeyO
-	case gxui.KeyP:
-		return glfw.KeyP
-	case gxui.KeyQ:
-		return glfw.KeyQ
-	case gxui.KeyR:
-		return glfw.KeyR
-	case gxui.KeyS:
-		return glfw.KeyS
-	case gxui.KeyT:
-		return glfw.KeyT
-	case gxui.KeyU:
-		return glfw.KeyU
-	case gxui.KeyV:
-		return glfw.KeyV
-	case gxui.KeyW:
-		return glfw.KeyW
-	case gxui.KeyX:
-		return glfw.KeyX
-	case gxui.KeyY:
-		return glfw.KeyY
-	case gxui.KeyZ:
-		return glfw.KeyZ
-	case gxui.KeyLeftBracket:
-		return glfw.KeyLeftBracket
-	case gxui.KeyBackslash:
-		return glfw.KeyBackslash
-	case gxui.KeyRightBracket:
-		return glfw.KeyRightBracket
-	case gxui.KeyGraveAccent:
-		return glfw.KeyGraveAccent
-	case gxui.KeyWorld1:
-		return glfw.KeyWorld1
-	case gxui.KeyWorld2:
-		return glfw.KeyWorld2
-	case gxui.KeyEscape:
-		return glfw.KeyEscape
-	case gxui.KeyEnter:
-		return glfw.KeyEnter
-	case gxui.KeyTab:
-		return glfw.KeyTab
-	case gxui.KeyBackspace:
-		return glfw.KeyBackspace
-	case gxui.KeyInsert:
-		return glfw.KeyInsert
-	case gxui.KeyDelete:
-		return glfw.KeyDelete
-	case gxui.KeyRight:
-		return glfw.KeyRight
-	case gxui.KeyLeft:
-		return glfw.KeyLeft
-	case gxui.KeyDown:
-		return glfw.KeyDown
-	case gxui.KeyUp:
-		return glfw.KeyUp
-	case gxui.KeyPageUp:
-		return glfw.KeyPageUp
-	case gxui.KeyPageDown:
-		return glfw.KeyPageDown
-	case gxui.KeyHome:
-		return glfw.KeyHome
-	case gxui.KeyEnd:
-		return glfw.KeyEnd
-	case gxui.KeyCapsLock:
-		return glfw.KeyCapsLock
-	case gxui.KeyScrollLock:
-		return glfw.KeyScrollLock
-	case gxui.KeyNumLock:
-		return glfw.KeyNumLock
-	case gxui.KeyPrintScreen:
-		return glfw.KeyPrintScreen
-	case gxui.KeyPause:
-		return glfw.KeyPause
-	case gxui.KeyF1:
-		return glfw.KeyF1
-	case gxui.KeyF2:
-		return glfw.KeyF2
-	case gxui.KeyF3:
-		return glfw.KeyF3
-	case gxui.KeyF4:
-		return glfw.KeyF4
-	case gxui.KeyF5:
-		return glfw.KeyF5
-	case gxui.KeyF6:
-		return glfw.KeyF6
-	case gxui.KeyF7:
-		return glfw.KeyF7
-	case gxui.KeyF8:
-		return glfw.KeyF8
-	case gxui.KeyF9:
-		return glfw.KeyF9
-	case gxui.KeyF10:
-		return glfw.KeyF10
-	case gxui.KeyF11:
-		return glfw.KeyF11
-	case gxui.KeyF12:
-		return glfw.KeyF12
-	case gxui.KeyKp0:
-		return glfw.KeyKP0
-	case gxui.KeyKp1:
-		return glfw.KeyKP1
-	case gxui.KeyKp2:
-		return glfw.KeyKP2
-	case gxui.KeyKp3:
-		return glfw.KeyKP3
-	case gxui.KeyKp4:
-		return glfw.KeyKP4
-	case gxui.KeyKp5:
-		return glfw.KeyKP5
-	case gxui.KeyKp6:
-		return glfw.KeyKP6
-	case gxui.KeyKp7:
-		return glfw.KeyKP7
-	case gxui.KeyKp8:
-		return glfw.KeyKP8
-	case gxui.KeyKp9:
-		return glfw.KeyKP9
-	case gxui.KeyKpDecimal:
-		return glfw.KeyKPDecimal
-	case gxui.KeyKpDivide:
-		return glfw.KeyKPDivide
-	case gxui.KeyKpMultiply:
-		return glfw.KeyKPMultiply
-	case gxui.KeyKpSubtract:
-		return glfw.KeyKPSubtract
-	case gxui.KeyKpAdd:
-		return glfw.KeyKPAdd
-	case gxui.KeyKpEnter:
-		return glfw.KeyKPEnter
-	case gxui.KeyKpEqual:
-		return glfw.KeyKPEqual
-	case gxui.KeyLeftShift:
-		return glfw.KeyLeftShift
-	case gxui.KeyLeftControl:
-		return glfw.KeyLeftControl
-	case gxui.KeyLeftAlt:
-		return glfw.KeyLeftAlt
-	case gxui.KeyLeftSuper:
-		return glfw.KeyLeftSuper
-	case gxui.KeyRightShift:
-		return glfw.KeyRightShift
-	case gxui.KeyRightControl:
-		return glfw.KeyRightControl
-	case gxui.KeyRightAlt:
-		return glfw.KeyRightAlt
-	case gxui.KeyRightSuper:
-		return glfw.KeyRightSuper
-	case gxui.KeyMenu:
-		return glfw.KeyMenu
-	default:
-		return glfw.KeyUnknown
+var KeyMap = map[gxui.KeyboardKey]int{
+	gxui.KeyUnknown:      0x0000,
+	gxui.KeyEscape:       0x0001,
+	gxui.Key1:            0x0002,
+	gxui.Key2:            0x0003,
+	gxui.Key3:            0x0004,
+	gxui.Key4:            0x0005,
+	gxui.Key5:            0x0006,
+	gxui.Key6:            0x0007,
+	gxui.Key7:            0x0008,
+	gxui.Key8:            0x0009,
+	gxui.Key9:            0x000A,
+	gxui.Key0:            0x000B,
+	gxui.KeyMinus:        0x000C,
+	gxui.KeyEqual:        0x000D,
+	gxui.KeyBackspace:    0x000E,
+	gxui.KeyTab:          0x000F,
+	gxui.KeyQ:            0x0010,
+	gxui.KeyW:            0x0011,
+	gxui.KeyE:            0x0012,
+	gxui.KeyR:            0x0013,
+	gxui.KeyT:            0x0014,
+	gxui.KeyY:            0x0015,
+	gxui.KeyU:            0x0016,
+	gxui.KeyI:            0x0017,
+	gxui.KeyO:            0x0018,
+	gxui.KeyP:            0x0019,
+	gxui.KeyLeftBracket:  0x001A,
+	gxui.KeyRightBracket: 0x001B,
+	gxui.KeyEnter:        0x001C,
+	gxui.KeyLeftControl:  0x001D,
+	gxui.KeyA:            0x001E,
+	gxui.KeyS:            0x001F,
+	gxui.KeyD:            0x0020,
+	gxui.KeyF:            0x0021,
+	gxui.KeyG:            0x0022,
+	gxui.KeyH:            0x0023,
+	gxui.KeyJ:            0x0024,
+	gxui.KeyK:            0x0025,
+	gxui.KeyL:            0x0026,
+	gxui.KeySemicolon:    0x0027,
+	//"Quote":              0x0028,
+	//"Backquote":          0x0029,
+	gxui.KeyLeftShift:  0x002A,
+	gxui.KeyBackslash:  0x002B,
+	gxui.KeyZ:          0x002C,
+	gxui.KeyX:          0x002D,
+	gxui.KeyC:          0x002E,
+	gxui.KeyV:          0x002F,
+	gxui.KeyB:          0x0030,
+	gxui.KeyN:          0x0031,
+	gxui.KeyM:          0x0032,
+	gxui.KeyComma:      0x0033,
+	gxui.KeyPeriod:     0x0034,
+	gxui.KeySlash:      0x0035,
+	gxui.KeyRightShift: 0x0036,
+	gxui.KeyKpMultiply: 0x0037,
+	gxui.KeyLeftAlt:    0x0038,
+	gxui.KeySpace:      0x0039,
+	gxui.KeyCapsLock:   0x003A,
+	gxui.KeyF1:         0x003B,
+	gxui.KeyF2:         0x003C,
+	gxui.KeyF3:         0x003D,
+	gxui.KeyF4:         0x003E,
+	gxui.KeyF5:         0x003F,
+	gxui.KeyF6:         0x0040,
+	gxui.KeyF7:         0x0041,
+	gxui.KeyF8:         0x0042,
+	gxui.KeyF9:         0x0043,
+	gxui.KeyF10:        0x0044,
+	//gxui.KeyPause:        0x0045,
+	gxui.KeyScrollLock:   0x0046,
+	gxui.KeyKp7:          0x0047,
+	gxui.KeyKp8:          0x0048,
+	gxui.KeyKp9:          0x0049,
+	gxui.KeyKpSubtract:   0x004A,
+	gxui.KeyKp4:          0x004B,
+	gxui.KeyKp5:          0x004C,
+	gxui.KeyKp6:          0x004D,
+	gxui.KeyKpAdd:        0x004E,
+	gxui.KeyKp1:          0x004F,
+	gxui.KeyKp2:          0x0050,
+	gxui.KeyKp3:          0x0051,
+	gxui.KeyKp0:          0x0052,
+	gxui.KeyKpDecimal:    0x0053,
+	gxui.KeyF11:          0x0057,
+	gxui.KeyF12:          0x0058,
+	gxui.KeyKpEqual:      0x0059,
+	gxui.KeyKpEnter:      0xE01C,
+	gxui.KeyRightControl: 0xE01D,
+	gxui.KeyKpDivide:     0xE035,
+	gxui.KeyPrintScreen:  0xE037,
+	gxui.KeyRightAlt:     0xE038,
+	gxui.KeyNumLock:      0xE045,
+	gxui.KeyPause:        0xE046,
+	gxui.KeyHome:         0xE047,
+	gxui.KeyUp:           0xE048,
+	gxui.KeyPageUp:       0xE049,
+	gxui.KeyLeft:         0xE04B,
+	gxui.KeyRight:        0xE04D,
+	gxui.KeyEnd:          0xE04F,
+	gxui.KeyDown:         0xE050,
+	gxui.KeyPageDown:     0xE051,
+	gxui.KeyInsert:       0xE052,
+	gxui.KeyDelete:       0xE053,
+	gxui.KeyMenu:         0xE05D,
+}
+
+func transKey(in gxui.KeyboardKey) int {
+	if v, ok := KeyMap[in]; ok {
+		return v
 	}
-}*/
+	return 0
+}
