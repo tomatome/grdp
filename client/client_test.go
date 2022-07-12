3a9c86ca221b8e6a853c126d60f487e935365b7c
tomatome <362521329@qq.com>
2022-07-12 11:38:13 +0800

    add client package

---
+++ client/client_test.go
@@ -0,0 +1,20 @@
+// client_test.go
+package client
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestClient(t *testing.T) {
+	c := NewClient("192.168.0.132:3389", "administrator", "Jhadmin123", TC_RDP, nil)
+	err := c.Login()
+	if err != nil {
+		fmt.Println("Login:", err)
+	}
+	c.OnBitmap(func(b []Bitmap) {
+		fmt.Println("ready:")
+	})
+	time.Sleep(100 * time.Second)
+}
