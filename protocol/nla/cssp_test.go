1538ae64282db45c9babc0d8c60b6005de11314c
Mikhail Tsyganov <tsyganov.michail@yandex.ru>
2022-11-16 20:09:27 +0300

    Fix cssp test

--- protocol/nla/cssp_test.go
+++ protocol/nla/cssp_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestEncodeDERTRequest(t *testing.T) {
 	ntlm := nla.NewNTLMv2("", "", "")
-	result := nla.EncodeDERTRequest([]nla.Message{ntlm.GetNegotiateMessage()}, "", "")
+	result := nla.EncodeDERTRequest([]nla.Message{ntlm.GetNegotiateMessage()}, []byte(""), []byte(""))
 	if hex.EncodeToString(result) != "302fa003020102a12830263024a02204204e544c4d53535000010000003582086000000000000000000000000000000000" {
 		t.Error("not equal")
 	}
