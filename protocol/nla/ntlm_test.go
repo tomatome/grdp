7939b56068bc2101999f72b1fac6b8b4a30b2e82
Mikhail Tsyganov <tsyganov.michail@yandex.ru>
2022-11-16 20:30:00 +0300

    Fix nla tests

--- protocol/nla/ntlm_test.go
+++ protocol/nla/ntlm_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/lunixbochs/struc"
+
 	"github.com/tomatome/grdp/protocol/nla"
 )
 
@@ -33,11 +34,11 @@ func TestNTLMv2_ComputeResponse(t *testing.T) {
 	Timestamp, _ := hex.DecodeString("a02f44f01267d501")
 	ServerName, _ := hex.DecodeString("02001e00570049004e002d00460037005200410041004d004100500034004a00430001001e00570049004e002d00460037005200410041004d004100500034004a00430004001e00570049004e002d00460037005200410041004d004100500034004a00430003001e00570049004e002d00460037005200410041004d004100500034004a00430007000800a02f44f01267d50100000000")
 
-	NtChallengeResponse, LmChallengeResponse, SessionBaseKey := ntlm.ComputeResponse(ResponseKeyNT, ResponseKeyLM, ServerChallenge, ClienChallenge, Timestamp, ServerName)
+	NtChallengeResponse, LmChallengeResponse, SessionBaseKey := ntlm.ComputeResponseV2(ResponseKeyNT, ResponseKeyLM, ServerChallenge, ClienChallenge, Timestamp, ServerName)
 
-	ntChallRespExpected := "4e7316531937d2fc91e7230853844b890101000000000000a02f44f01267d5011a78bed8e5d5efa70000000002001e00570049004e002d00460037005200410041004d004100500034004a00430001001e00570049004e002d00460037005200410041004d004100500034004a00430004001e00570049004e002d00460037005200410041004d004100500034004a00430003001e00570049004e002d00460037005200410041004d004100500034004a00430007000800a02f44f01267d50100000000"
+	ntChallRespExpected := "ea942653ab115d382d9206f1fe9d44d60101000000000000a02f44f01267d5011a78bed8e5d5efa70000000002001e00570049004e002d00460037005200410041004d004100500034004a00430001001e00570049004e002d00460037005200410041004d004100500034004a00430004001e00570049004e002d00460037005200410041004d004100500034004a00430003001e00570049004e002d00460037005200410041004d004100500034004a00430007000800a02f44f01267d5010000000000000000"
 	lmChallRespExpected := "d4dc6edc0c37dd70f69b5c4f05a615661a78bed8e5d5efa7"
-	sessBaseKeyExpected := "034009be89a0507b2bd6d28e966e1dab"
+	sessBaseKeyExpected := "0f400e7b256b77f28a5c7ff5e40e82b9"
 
 	if hex.EncodeToString(NtChallengeResponse) != ntChallRespExpected {
 		t.Error("NtChallengeResponse incorrect")
@@ -49,14 +50,5 @@ func TestNTLMv2_ComputeResponse(t *testing.T) {
 
 	if hex.EncodeToString(SessionBaseKey) != sessBaseKeyExpected {
 		t.Error("SessionBaseKey incorrect")
-	}
-}
-
-func TestSIGNKEY(t *testing.T) {
-	exportedSessionKey, _ := hex.DecodeString("be32c3c56ea6683200a35329d67880c3")
-	result := hex.EncodeToString(nla.SIGNKEY(exportedSessionKey, true))
-	expected := "79b4f9a4113230f378a0af99f784adae"
-	if result != expected {
-		t.Error(result, "not equal to", expected)
 	}
 }
